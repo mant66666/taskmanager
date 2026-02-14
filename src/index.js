36f407b899dc7bf5aed125c2d261f2e36c3170d4
mant66666 <egor.sudakov@gmail.com>
2026-02-14 19:50:25 +0300

    разделил dist и src подключил package.json и npm run

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './main.scss';
+import '../dev/cases.css';
+import '../dev/news.css';
+import '../dev/startscreen.css';
+import '../dev/style.css';
+import '../dev/cases.js';
+import '../dev/startscreen.js';
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', // точка входа
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[contenthash].js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[hash][ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './dev/index.html', // твой HTML из папки dev
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: 'dev/img', 
+          to: 'img',
+          noErrorOnMissing: true 
+        }
+      ]
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    port: 3000,
+    hot: true,
+    open: true,
+    compress: true
+  }
+};
